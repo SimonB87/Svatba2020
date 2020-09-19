fcaed1ec1785be57ccd20b0f4063de97e44940b6
SimonB87 <buryans@gmail.com>
2020-09-19 18:12:45 +0200

    photo edits

--- js/main.js
+++ js/main.js
@@ -56,10 +56,10 @@
     /*------------------
         Background Set
     --------------------*/
-    $('.set-bg').each(function () {
+/*     $('.set-bg').each(function () {
         var bg = $(this).data('setbg');
         $(this).css('background-image', 'url(' + bg + ')');
-    });
+    }); */
 
     // Search model
     $('.search-switch').on('click', function () {
